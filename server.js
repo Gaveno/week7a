d5bf27bab32533fe8870de1f4d6434af94591d1a
Gavin <gavin.monkeystudios@gmail.com>
2019-03-17 09:48:18 -0600

    Added Bad Route Catches
    - Added error catches to invalid operations or paths.

--- server.js
+++ server.js
@@ -25,7 +25,11 @@ router.route('/postjwt')
             }
             res.send(req.body);
         }
-    );
+    )
+    .all(function (req, res) {
+        console.log(req.body);
+        res.status(403).send({ success: false, message: "Operation not supported. Only POST allowed." });
+    });
 
 router.route('/users/:userId')
     .get(authJwtController.isAuthenticated, function (req, res) {
@@ -37,6 +41,10 @@ router.route('/users/:userId')
             // return that user
             res.json(user);
         });
+    })
+    .all(function (req, res) {
+        console.log(req.body);
+        res.status(403).send({ success: false, message: "Operation not supported. Only GET allowed." });
     });
 
 router.route('/users')
@@ -46,56 +54,70 @@ router.route('/users')
             // return the users
             res.json(users);
         });
+    })
+    .all(function (req, res) {
+        console.log(req.body);
+        res.status(403).send({ success: false, message: "Operation not supported. Only GET allowed." });
     });
 
-router.post('/signup', function(req, res) {
-    if (!req.body.username || !req.body.password) {
-        res.json({success: false, message: 'Please pass username and password.'});
-    }
-    else {
-        var user = new User();
-        user.name = req.body.name;
-        user.username = req.body.username;
-        user.password = req.body.password;
-        // save the user
-        user.save(function(err) {
-            if (err) {
-                // duplicate entry
-                if (err.code == 11000)
-                    return res.json({ success: false, message: 'A user with that username already exists. '});
-                else
-                    return res.send(err);
-            }
-
-            res.json({ success: true, message: 'User created!' });
-        });
-    }
-});
-
-router.post('/signin', function(req, res) {
-    var userNew = new User();
-    userNew.name = req.body.name;
-    userNew.username = req.body.username;
-    userNew.password = req.body.password;
-
-    User.findOne({ username: userNew.username }).select('name username password').exec(function(err, user) {
-        if (err) res.send(err);
-        if (user) {
-            user.comparePassword(userNew.password, function (isMatch) {
-                if (isMatch) {
-                    var userToken = {id: user._id, username: user.username};
-                    var token = jwt.sign(userToken, process.env.SECRET_KEY);
-                    res.json({success: true, token: 'JWT ' + token});
-                } else {
-                    res.status(401).send({success: false, message: 'Authentication failed.'});
-                }
-            });
+router.route('/signup')
+    .post(function(req, res) {
+        if (!req.body.username || !req.body.password) {
+            res.json({success: false, message: 'Please pass username and password.'});
         }
         else {
-            return res.status(403).send({ success: false, message: "User not found." });
+            var user = new User();
+            user.name = req.body.name;
+            user.username = req.body.username;
+            user.password = req.body.password;
+            // save the user
+            user.save(function(err) {
+                if (err) {
+                    // duplicate entry
+                    if (err.code == 11000)
+                        return res.json({ success: false, message: 'A user with that username already exists. '});
+                    else
+                        return res.send(err);
+                }
+
+                res.json({ success: true, message: 'User created!' });
+            });
         }
+    })
+    .all(function (req, res) {
+        console.log(req.body);
+        res.status(403).send({ success: false, message: "Operation not supported. Only POST allowed." });
     });
-});
+
+router.route('/signin')
+    .post(function(req, res) {
+        var userNew = new User();
+        userNew.name = req.body.name;
+        userNew.username = req.body.username;
+        userNew.password = req.body.password;
+
+        User.findOne({ username: userNew.username }).select('name username password').exec(function(err, user) {
+            if (err) res.send(err);
+            if (user) {
+                user.comparePassword(userNew.password, function (isMatch) {
+                    if (isMatch) {
+                        var userToken = {id: user._id, username: user.username};
+                        var token = jwt.sign(userToken, process.env.SECRET_KEY);
+                        res.json({success: true, token: 'JWT ' + token});
+                    } else {
+                        res.status(401).send({success: false, message: 'Authentication failed.'});
+                    }
+                });
+            }
+            else {
+                return res.status(403).send({ success: false, message: "User not found." });
+            }
+        });
+    })
+    .all(function (req, res) {
+        console.log(req.body);
+        res.status(403).send({ success: false, message: "Operation not supported. Only POST allowed." });
+    });
 
 router.route('/movies')
     .post(authJwtController.isAuthenticated, function (req, res) {
@@ -176,6 +198,16 @@ router.route('/movies')
                 else res.status(200).json({ success: true, message: "Successfully deleted.", numberDeleted: doc.n });
             })
         }
+    })
+    .all(function (req, res) {
+        console.log(req.body);
+        res.status(403).send({ success: false, message: "Operation not supported. Only POST, PUT, GET, and DELETE allowed." });
+    });
+
+router.route('/')
+    .all(function (req, res) {
+        console.log(req.body);
+        res.status(404).send({ success: false, message: "Invalid path."});
     });
 
 app.use('/', router);
